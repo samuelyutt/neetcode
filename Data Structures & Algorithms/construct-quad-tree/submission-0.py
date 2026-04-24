74e7ef58383b0c970eecafec23d24c58d4e0e8fe
Samuel Yu <43903194+samuelyutt@users.noreply.github.com>
2026-04-24 15:22:08 +0800

    Add: construct-quad-tree - submission-0

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-0.py
@@ -0,0 +1,45 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        n = len(grid)
+
+        memo = {} # (x, y, side) -> Node
+        for i in range(n):
+            for j in range(n):
+                memo[(i, j, 1)] = Node(grid[i][j], 1, None, None, None, None)
+
+        side = 1
+        while side < n:
+            next_side = side * 2
+            for i in range(0, n, next_side):
+                for j in range(0, n, next_side):
+                    tl = memo[(i, j, side)]
+                    tr = memo[(i, j + side, side)]
+                    bl = memo[(i + side, j, side)]
+                    br = memo[(i + side, j + side, side)]
+                    sum_val = sum([tl.val, tr.val, bl.val, br.val])
+                    all_leaf = sum([tl.isLeaf, tr.isLeaf, bl.isLeaf, br.isLeaf]) == 4
+                    if all_leaf and (sum_val == 0 or sum_val == 4):
+                        memo[(i, j, next_side)] = Node(tl.val, 1, None, None, None, None)
+                    else:
+                        memo[(i, j, next_side)] = Node(1, 0, tl, tr, bl, br)
+            side = next_side
+
+        return memo[(0, 0, n)]
+
+
+                    
+
+                    
+                    
