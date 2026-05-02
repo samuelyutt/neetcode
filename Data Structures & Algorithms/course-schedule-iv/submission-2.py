519b42661476c3dd5ceec8d1c8522ce032067264
Samuel Yu <43903194+samuelyutt@users.noreply.github.com>
2026-05-02 22:26:46 +0800

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        forward = defaultdict(set)
+        backward = defaultdict(set)
+        # tails = set([i for i in range(numCourses)])
+        outdegree = [0] * numCourses
+        for prereq in prerequisites:
+            a, b = prereq[0], prereq[1]
+            forward[a].add(b)
+            backward[b].add(a)
+            # tails.discard(a)
+            outdegree[a] += 1
+
+        # courses = deque([course for course in tails])
+        courses = deque([i for i in range(numCourses) if outdegree[i] == 0])
+        while courses:
+            b = courses.popleft()
+            for a in backward[b]:
+                forward[a].update(forward[b])
+                outdegree[a] -= 1
+                if outdegree[a] == 0:
+                    courses.append(a)
+
+        ret = []
+        for query in queries:
+            u, v = query[0], query[1]
+            ret.append(v in forward[u])
+        return ret
