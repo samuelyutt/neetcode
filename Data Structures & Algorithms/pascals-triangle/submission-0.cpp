bb16208c1bc474374b80253a498cbca3ecf4a47a
Samuel Yu <43903194+samuelyutt@users.noreply.github.com>
2026-05-18 15:07:44 +0800

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret;
+        const vector<int>& prev = ret.back();
+        ret.push_back({1});
+        for (int i = 1; i < numRows; i++) {
+            // vector<int> prev = ret.back(); // This creates a copy of the last vector.
+            const vector<int>& prev = ret.back(); // a reference (no copy)
+
+            vector<int> cur = {1};
+            for (int j = 0; j < prev.size() - 1; j++) {
+                cur.push_back(prev[j] + prev[j + 1]);
+            }
+            cur.push_back(1);
+            ret.push_back(cur);
+        }
+        return ret;
+    }
+};
