633c4956955ca0b62de87e466ef852038f82640c
Samuel Yu <43903194+samuelyutt@users.noreply.github.com>
2026-06-20 16:11:59 +0800

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,36 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node(c)
+            node = node.children[c]
+        node.is_leaf = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_leaf
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+        
+        
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.children = {}
+        self.is_leaf = False
