076b228aae34d47f234eafc6474f821520cf03b1
Samuel Yu <43903194+samuelyutt@users.noreply.github.com>
2026-07-15 17:54:35 +0800

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.contains(target - nums[i])) {
+                return {m[target - nums[i]], i};
+            }
+            m[nums[i]] = i;
+        }
+    }
+};
